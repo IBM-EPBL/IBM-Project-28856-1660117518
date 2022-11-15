4f5bb1deacf7a7555af72c221ddcd1aa1d552f54
Siddharthkumarmishra3 <115971270+Siddharthkumarmishra3@users.noreply.github.com>
2022-11-15 12:06:08 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 1/myapp/myapp/urls.py
@@ -0,0 +1,10 @@
+from django import views
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/',include('api.urls')),
+    path('register',views.register),
+]
